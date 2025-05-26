390722333ac878d7f0fd3ece5744e78e2217e671
dcrispim <diego.crispim@yahoo.com.br>
2025-05-25 21:14:09 -0300

    main page

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+declare type Detection = {
+  id: number;
+  name: string;
+  age: number;
+  time: string;
+  confidence: number;
+  mood: string;
+  gender: string;
+  x?: number;
+  y?: number;
+};
